fda39f3012991e4830af1371315ab8958a6f7f8d
Felipe Souza <fbsdsgn@gmail.com>
2022-05-19 14:01:10 -0300

    add component styled Profile

--- src/components/Profile/styles.ts
+++ src/components/Profile/styles.ts
@@ -4,43 +4,102 @@ import profile from "../../assets/img/profile.png";
 
 export const Content = styled("div", {
   position: "relative",
-  paddingTop: "3.5rem",
+  mt: "$18",
 });
 
 export const Data = styled("div", {
   display: "grid",
   textAlign: "center",
+  justifyContent: "center",
 });
 
-export const Border = styled("div", {});
+export const Border = styled("div", {
+  display: "grid",
+  size: "$20",
+  border: "4px solid $blue11",
+  justifySelf: "center",
+  borderRadius: "50%",
+  placeItems: "center",
+  mb: "$4",
+});
 
 export const Perfil = styled("div", {
-  width: 100,
-  height: 100,
+  display: "grid",
+  size: "$18",
   borderRadius: "50%",
   overflow: "hidden",
-  display: "flex",
-  alignItems: "flex-end",
+  alignItems: "flex-center",
   justifyContent: "center",
 });
 
 export const Image = styled("div", {
   backgroundImage: `url(${profile})`,
-  width: 105,
-  height: 105,
+  width: "$18",
   backgroundPosition: "center",
-  backgroundSize: "105px",
+  backgroundSize: "160px",
   backgroundRepeat: "no-repeat",
 });
 
-export const Name = styled("h2", {});
+export const Name = styled("h2", {
+  fontSize: "$4",
+  mb: "$1",
+  color: "$blue12",
+  fontWeight: "500",
+});
 
-export const Profession = styled("h3", {});
+export const Profession = styled("h3", {
+  fontSize: "$2",
+  mb: "$4",
+  color: "$blue11",
+  fontWeight: "lighter",
+});
 
-export const Social = styled("ul", {});
+export const Social = styled("ul", {
+  display: "flex",
+  width: 280,
+  jc: "space-between",
+  columnGap: ".75rem",
+  mb: "$4",
+});
 
-export const Li = styled("li", {});
+export const Li = styled("li", {
+  size: "$6",
+  display: "flex",
+  jc: "center",
+  ai: "center",
+  borderRadius: 4,
+  transition: "0.3s",
+  background: "$blue2",
+  "&:hover": {
+    background: "$blue3",
+  },
+});
 
-export const Link = styled("a", {});
+export const Link = styled("a", {
+  color: "$blue12",
+  padding: "$1",
+  transition: "0.3s",
+  "&:hover": {
+    color: "$blue11",
+  },
+});
 
-export const Button = styled("a", {});
+export const Button = styled("a", {
+  display: "flex",
+  jc: "center",
+  ai: "center",
+  m: "0 auto",
+
+  fontSize: "$3",
+  padding: "$2 $3",
+  columnGap: "$1",
+
+  borderRadius: 4,
+
+  color: "$blue12",
+  backgroundColor: "$blue8",
+  transition: "0.3s",
+  "&:hover": {
+    backgroundColor: "$blue7",
+  },
+});
