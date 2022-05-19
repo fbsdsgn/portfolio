e6bfb88cb2066265d2da67d882a2900893f784ce
Felipe Souza <fbsdsgn@gmail.com>
2022-05-19 13:59:48 -0300

    add component styled Main

--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -1,13 +1,13 @@
 import { styled } from "../../styles/config/stitches.config";
 
-
 export const Container = styled("main", {});
 
 export const Filters = styled("ul", {});
 
 export const Button = styled("button", {});
 
-export const Content = styled("div", {})
+export const Content = styled("div", {
+  mt: "$9",
+});
 
 export const Sections = styled("div", {});
-
