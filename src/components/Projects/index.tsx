a00c53474c3b8b4f86c18a19e12f83953cda4855
Felipe Souza <fbsdsgn@gmail.com>
2022-05-22 15:26:53 -0300

    feat: update component and styled Projects

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,18 +1,57 @@
 import { Container } from "../../styles/global";
 
-import { Card, Image, Modal, Divide, Subtitle, Title, Link } from "./styles"
+import { api } from "../../api/config";
+
+api.get("/profile").then((res) => console.log(res));
+
+import {
+  Card,
+  Image,
+  Modal,
+  Divide,
+  Subtitle,
+  Title,
+  Link,
+  Wrapper,
+  Content,
+  Group,
+} from "./styles";
+import { FigmaLogo, GithubLogo, LinkSimple } from "phosphor-react";
+
+import { DATA } from "../../helpers/data";
+
+const projects = DATA[0]["projects"];
 
 export const Projects = () => (
   <Container>
-    <Card>
-      <Image src="#" />
-      <Modal>
-        <Divide>
-          <Subtitle>UI Design</Subtitle>
-          <Title>Kenzie Hub</Title>
-          <Link href="#">Icon Link</Link>
-        </Divide>
-      </Modal>
-    </Card>
+    <Content>
+      {projects.map(
+        ({ title, subtitle, deployURL, githubURL, figmaURL, imageURL }) => (
+          <Card key={title}>
+            <Image src={imageURL} />
+            <Modal>
+              <Divide>
+                <Subtitle>{subtitle}</Subtitle>
+                <Title>{title}</Title>
+                <Wrapper>
+                  <Link href={deployURL}>
+                    <LinkSimple size={32} />
+                  </Link>
+                  <Group>
+                    <Link color="secondary" size="small" href={githubURL}>
+                      <GithubLogo size={24} />
+                    </Link>
+
+                    <Link color="secondary" size="small" href={figmaURL}>
+                      <FigmaLogo size={24} />
+                    </Link>
+                  </Group>
+                </Wrapper>
+              </Divide>
+            </Modal>
+          </Card>
+        )
+      )}
+    </Content>
   </Container>
 );
--- src/components/Projects/styles.ts
+++ src/components/Projects/styles.ts
@@ -1,17 +1,89 @@
 import { styled } from "../../styles/config/stitches.config";
 
-export const Projects = styled("div", {});
+export const Content = styled("div", {
+  display: "flex",
+  columnGap: "$3",
+  rowGap: "$3",
+  flexWrap: "wrap",
+  jc: "center",
+});
 
-export const Card = styled("article", {});
+export const Card = styled("article", {
+  position: "relative",
+  borderRadius: 4,
+  overflow: "hidden",
+  "&:hover div": {
+    bottom: 0,
+  },
+});
 
-export const Image = styled("img", {});
+export const Image = styled("img", {
+  size: "250px",
+  borderRadius: 4,
+});
 
-export const Modal = styled("div", {});
+export const Modal = styled("div", {
+  position: "absolute",
+  width: "100%",
+  height: "100%",
+  bottom: "-100%",
+  left: 0,
+  display: "grid",
+  alignItems: "flex-end",
+  padding: "$2",
+
+  borderRadius: 4,
+  transition: ".3s",
+  background:
+    "linear-gradient(360deg, #000000 -7.6%, rgba(0, 34, 74, 0.31) 102.14%)",
+});
 
 export const Divide = styled("div", {});
 
-export const Subtitle = styled("span", {});
+export const Group = styled("div", {
+  display: "flex",
+  columnGap: "$2",
+});
 
-export const Title = styled("h3", {});
+export const Wrapper = styled("div", {
+  display: "flex",
+  jc: "space-between",
+  ai: "flex-end",
+});
 
-export const Link = styled("a", {});
+export const Subtitle = styled("h4", {
+  color: "$blue12",
+  mb: "$1",
+  fontWeight: "lighter",
+  fontSize: "$2",
+});
+
+export const Title = styled("h3", {
+  fontSize: "$3",
+  color: "$blue12",
+  mb: "$2",
+});
+
+export const Link = styled("a", {
+  size: "$5",
+  backgroundColor: "$blue7",
+  borderRadius: 4,
+  display: "flex",
+  jc: "center",
+  ai: "center",
+  color: "$blue12",
+
+  variants: {
+    size: {
+      small: {
+        size: "$4",
+      },
+    },
+
+    color: {
+      secondary: {
+        backgroundColor: "$blue2",
+      },
+    },
+  },
+});
