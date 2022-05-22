6439bdbc6eb2d537753638f23bfe5a62a0521338
Felipe Souza <fbsdsgn@gmail.com>
2022-05-22 15:26:40 -0300

    feat: update component and styled Main

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,20 +1,46 @@
+import { FacebookLogo, InstagramLogo } from "phosphor-react";
+import { useState } from "react";
+
 import { Container } from "../../styles/global";
-import { Projects} from "../Projects" 
+import { Projects } from "../Projects";
 import { Skills } from "../Skills";
-import { Button, Content, Sections } from "./styles";
+import { Button, Content, Link, Sections, Social } from "./styles";
 
 export const Main = () => {
+  const [toggleState, setToggleState] = useState(1);
+
+  const toggleTab = (index: number) => {
+    setToggleState(index);
+  };
+
   return (
     <Container>
       <Content>
-        <Button>Projects</Button>
-        <Button>Skills</Button>
+        <Button
+          onClick={() => toggleTab(1)}
+          className={toggleState === 1 ? "active" : ""}
+        >
+          Projects
+        </Button>
+        <Button
+          onClick={() => toggleTab(2)}
+          className={toggleState === 2 ? "active" : ""}
+        >
+          Skills
+        </Button>
       </Content>
 
-      <Sections>
-        <Projects />
-        <Skills />
-      </Sections>
+      <Sections>{toggleState === 1 ? <Projects /> : <Skills />}</Sections>
+
+      <Social>
+        <Link href="https://fb.me/fbsdsgn" target="_blank">
+          <InstagramLogo size={24} />
+        </Link>
+
+        <Link href="https://instagram.com/fbsdsgn" target="_blank">
+          <FacebookLogo size={24} />
+        </Link>
+      </Social>
     </Container>
   );
 };
--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -1,13 +1,51 @@
 import { styled } from "../../styles/config/stitches.config";
 
-export const Container = styled("main", {});
-
 export const Filters = styled("ul", {});
 
-export const Button = styled("button", {});
+export const Button = styled("button", {
+  width: "$19",
+  fontSize: "$2",
+  padding: "$2",
+  borderRadius: 4,
+  border: "none",
+  backgroundColor: "transparent",
+  color: "$blue12",
+  transition: "0.3s",
 
-export const Content = styled("div", {
-  mt: "$9",
+  "&.active": {
+    backgroundColor: "$blue1",
+  },
+
+  "&:hover": {
+    backgroundColor: "$blue1",
+  },
 });
 
-export const Sections = styled("div", {});
+export const Content = styled("div", {
+  display: "flex",
+  columnGap: "$1",
+  m: "$6 auto",
+  padding: "$1",
+  backgroundColor: "$blue2",
+  borderRadius: 4,
+});
+
+export const Sections = styled("div", {
+  mb: "$4"
+});
+
+export const Social = styled("div", {
+  textAlign: "center",
+  mb: "$8"
+});
+
+export const Link = styled("a", {
+  color: "$blue12",
+  padding: 2,
+  margin: 2,
+  transition: "0.3s",
+
+  "&:hover": {
+    color: "$blue9",
+  },
+});
