d22d3d47c3a3765d0ba812f1c9383fd8d57d4685
Felipe Souza <fbsdsgn@gmail.com>
2022-05-23 19:43:40 -0300

    update data.ts

--- src/helpers/data.ts
+++ src/helpers/data.ts
@@ -2,6 +2,7 @@ export const DATA = [
   {
     projects: [
       {
+        id: 1,
         title: "Kenzie Hub",
         subtitle: "Front-end",
         deployURL: "https://github.com/fbsdsgn",
@@ -11,6 +12,17 @@ export const DATA = [
           "https://scontent.fssz7-1.fna.fbcdn.net/v/t39.30808-6/274550265_156639246726000_844590552053243511_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=lV09g3I2GdYAX9D_4TI&_nc_ht=scontent.fssz7-1.fna&oh=00_AT-2kU5_d_6ARQPCvF9_yuYTSzyOup7b8KbTGZ3sLTkI5A&oe=628EB91C",
       },
       {
+        id: 2,
+        title: "Blog",
+        subtitle: "Front-end",
+        deployURL: "https://github.com/fbsdsgn",
+        githubURL: "",
+        figmaURL: "",
+        imageURL:
+          "https://scontent.fssz7-1.fna.fbcdn.net/v/t39.30808-6/274550265_156639246726000_844590552053243511_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=lV09g3I2GdYAX9D_4TI&_nc_ht=scontent.fssz7-1.fna&oh=00_AT-2kU5_d_6ARQPCvF9_yuYTSzyOup7b8KbTGZ3sLTkI5A&oe=628EB91C",
+      },
+      {
+        id: 3,
         title: "Kenzie Hub",
         subtitle: "Front-end",
         deployURL: "https://github.com/fbsdsgn",
@@ -20,6 +32,7 @@ export const DATA = [
           "https://scontent.fssz7-1.fna.fbcdn.net/v/t39.30808-6/274550265_156639246726000_844590552053243511_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=lV09g3I2GdYAX9D_4TI&_nc_ht=scontent.fssz7-1.fna&oh=00_AT-2kU5_d_6ARQPCvF9_yuYTSzyOup7b8KbTGZ3sLTkI5A&oe=628EB91C",
       },
       {
+        id: 4,
         title: "Kenzie Hub",
         subtitle: "Front-end",
         deployURL: "https://github.com/fbsdsgn",
@@ -29,6 +42,7 @@ export const DATA = [
           "https://scontent.fssz7-1.fna.fbcdn.net/v/t39.30808-6/274550265_156639246726000_844590552053243511_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=lV09g3I2GdYAX9D_4TI&_nc_ht=scontent.fssz7-1.fna&oh=00_AT-2kU5_d_6ARQPCvF9_yuYTSzyOup7b8KbTGZ3sLTkI5A&oe=628EB91C",
       },
       {
+        id: 5,
         title: "Kenzie Hub",
         subtitle: "Front-end",
         deployURL: "https://github.com/fbsdsgn",
@@ -38,53 +52,58 @@ export const DATA = [
           "https://scontent.fssz7-1.fna.fbcdn.net/v/t39.30808-6/274550265_156639246726000_844590552053243511_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=lV09g3I2GdYAX9D_4TI&_nc_ht=scontent.fssz7-1.fna&oh=00_AT-2kU5_d_6ARQPCvF9_yuYTSzyOup7b8KbTGZ3sLTkI5A&oe=628EB91C",
       },
       {
+        id: 6,
         title: "Kenzie Hub",
         subtitle: "Front-end",
         deployURL: "https://github.com/fbsdsgn",
         githubURL: "",
         figmaURL: "",
-        imageURL: "https://scontent.fssz7-1.fna.fbcdn.net/v/t39.30808-6/274550265_156639246726000_844590552053243511_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=lV09g3I2GdYAX9D_4TI&_nc_ht=scontent.fssz7-1.fna&oh=00_AT-2kU5_d_6ARQPCvF9_yuYTSzyOup7b8KbTGZ3sLTkI5A&oe=628EB91C"
-      },
-      {
-        title: "Kenzie Hub",
-        subtitle: "Front-end",
-        deployURL: "https://github.com/fbsdsgn",
-        githubURL: "",
-        figmaURL: "",
-        imageURL: "https://scontent.fssz7-1.fna.fbcdn.net/v/t39.30808-6/274550265_156639246726000_844590552053243511_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=lV09g3I2GdYAX9D_4TI&_nc_ht=scontent.fssz7-1.fna&oh=00_AT-2kU5_d_6ARQPCvF9_yuYTSzyOup7b8KbTGZ3sLTkI5A&oe=628EB91C"
-      },
-      {
-        title: "Kenzie Hub",
-        subtitle: "Front-end",
-        deployURL: "https://github.com/fbsdsgn",
-        githubURL: "",
-        figmaURL: "",
-        imageURL: "https://scontent.fssz7-1.fna.fbcdn.net/v/t39.30808-6/274550265_156639246726000_844590552053243511_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=lV09g3I2GdYAX9D_4TI&_nc_ht=scontent.fssz7-1.fna&oh=00_AT-2kU5_d_6ARQPCvF9_yuYTSzyOup7b8KbTGZ3sLTkI5A&oe=628EB91C"
+        imageURL:
+          "https://scontent.fssz7-1.fna.fbcdn.net/v/t39.30808-6/274550265_156639246726000_844590552053243511_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=lV09g3I2GdYAX9D_4TI&_nc_ht=scontent.fssz7-1.fna&oh=00_AT-2kU5_d_6ARQPCvF9_yuYTSzyOup7b8KbTGZ3sLTkI5A&oe=628EB91C",
       },
     ],
-    skills: [
-      {
-        name: "",
-        level: "",
+    skills: {
+      front: {
+        colOne: [
+          { id: 1, name: "HTML", level: "Intermediate", icon: "" },
+          { id: 2, name: "CSS", level: "Intermediate", icon: "" },
+          { id: 3, name: "JavaScript", level: "Intermediate", icon: "" },
+        ],
+        colTwo: [
+          { id: 4, name: "ReactJS", level: "Intermediate", icon: "" },
+          { id: 5, name: "Tailwind", level: "Intermediate", icon: "" },
+          { id: 6, name: "Bootstrap", level: "Intermediate", icon: "" },
+        ],
       },
-    ],
-    profile: [
-      {
-        imageURL: "",
-        name: "Felipe B. Souza",
-        profession: "Web Developer",
-        social: {
-          github: "",
-          linkedin: "",
-          facebook: "",
-          instagram: "",
-        },
-        contact: {
-          whatsapp: "",
-          messenger: "",
-          phone: "",
-        },
+      back: {
+        colOne: [
+          { id: 1, name: "NodeJS", level: "Intermediate", icon: "" },
+          { id: 2, name: "Python", level: "Intermediate", icon: "" },
+          { id: 3, name: "Django", level: "Intermediate", icon: "" },
+        ],
+        colTwo: [
+          { id: 4, name: "Flask", level: "Intermediate", icon: "" },
+          { id: 5, name: "Docker", level: "Intermediate", icon: "" },
+          { id: 6, name: "AWS", level: "Intermediate", icon: "" },
+        ],
       },
-    ],
+    },
+
+    profile: {
+      imageURL: "",
+      fullname: "Felipe B. Souza",
+      profession: "Web Developer",
+      social: {
+        github: "",
+        linkedin: "",
+        facebook: "",
+        instagram: "",
+      },
+      contact: {
+        whatsapp: "",
+        messenger: "",
+        phone: "",
+      },
+    },
   },
 ];
